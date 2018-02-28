66ca1c433bee23509f461cb340b20bb1f8694f10
HyanCat <hyancat@live.cn>
2018-02-28 14:53:35 +0800

    Init composer.

---
+++ src/AIToyServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This file is part of ruogoo.
+ *
+ * Created by HyanCat.
+ *
+ * Copyright (C) HyanCat. All rights reserved.
+ */
+
+namespace Ruogoo\AIToy;
+
+use Illuminate\Support\ServiceProvider;
+
+class AIToyServiceProvider extends ServiceProvider {
+
+    protected $defer = true;
+
+    public function register() {
+        //
+    }
+
+    public function boot() {
+        //
+    }
+}
